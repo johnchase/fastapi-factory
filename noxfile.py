1efe46befd648fbf7ae5712a7d4675b32585946e
johnchase <johnhchase@protonmail.com>
2022-07-15 12:03:50 -0700

    test(noxfile.py): Add noxfile

---
+++ noxfile.py
@@ -0,0 +1,38 @@
+import nox
+
+
+@nox.session(python=["3.9"])
+def test(session):
+    session.install("poetry")
+    session.run("poetry", "lock", "--no-update")
+    session.run(
+        "poetry",
+        "install",
+    )
+    session.run("pytest", "--cov-report=term-missing", "--cov=app", "app/tests")
+
+
+@nox.session
+def coverage(session):
+    session.install("poetry")
+    session.run("poetry", "lock", "--no-update")
+    session.run("poetry", "install")
+    session.run("coverage", "report")
+
+
+@nox.session
+def lint(session):
+    session.install("poetry")
+    session.run("poetry", "lock", "--no-update")
+    session.run("poetry", "install")
+    session.run("black", "./app")
+    session.run("flake8")
+    session.run("pytest", "--isort")
+
+
+@nox.session
+def typing(session):
+    session.install("poetry")
+    session.run("poetry", "lock", "--no-update")
+    session.run("poetry", "install")
+    session.run("pyright", "./app")
