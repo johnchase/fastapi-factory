33868af93a3b549d7677ae29598ea100cd9b6df4
johnchase <johnhchase@protonmail.com>
2022-07-15 14:16:53 -0700

    bad

--- app/schemas/item.py
+++ app/schemas/item.py
@@ -5,35 +5,49 @@ from pydantic import BaseModel
 
 # Shared properties
 class ItemBase(BaseModel):
+    """ItemBase."""
+
     title: Optional[str] = None
     description: Optional[str] = None
 
 
 # Properties to receive on item creation
 class ItemCreate(ItemBase):
+    """ItemCreate."""
+
     title: str
 
 
 # Properties to receive on item update
 class ItemUpdate(ItemBase):
+    """ItemUpdate."""
+
     pass
 
 
 # Properties shared by models stored in DB
 class ItemInDBBase(ItemBase):
+    """ItemInDBBase."""
+
     id: int
     title: str
     owner_id: int
 
     class Config:
+        """Config."""
+
         orm_mode = True
 
 
 # Properties to return to client
 class Item(ItemInDBBase):
+    """Item."""
+
     pass
 
 
 # Properties properties stored in DB
 class ItemInDB(ItemInDBBase):
+    """ItemInDB."""
+
     pass
