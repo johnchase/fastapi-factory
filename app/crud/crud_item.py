1a1605c566431931812e84d4a26484aa0d3936f2
johnchase <johnhchase@protonmail.com>
2022-07-15 13:57:42 -0700

    fix(Typing): Not really a bug fix but needed a change

--- app/crud/crud_item.py
+++ app/crud/crud_item.py
@@ -29,7 +29,7 @@ class CRUDItem(CRUDBase[Item, ItemCreate, ItemUpdate]):
 
         """
         obj_in_data = jsonable_encoder(obj_in)
-        db_obj = self.model(**obj_in_data, owner_id=owner_id)
+        db_obj = self.model(**obj_in_data, owner_id=owner_id)  # type: ignore
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
