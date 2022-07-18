d4e8780ad24eae2fec6ab146a70e8729bc2196cb
johnchase <johnhchase@protonmail.com>
2022-07-18 14:22:30 -0700

    feat(Version): Adds dynamic version number

---
+++ app/__init__.py
@@ -0,0 +1,2 @@
+"""Initialize."""
+__version__ = "0.3.1"
--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from app import __version__
 from app.api.api_v1.api import api_router
 from app.core.config import settings
 
-app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
+app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json", version=__version__)
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
